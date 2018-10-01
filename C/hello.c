d1eab217f34fdc97da39b9a6dd1c2fa6af54959b
yourihdx <yourihdx@gmail.com>
2018-10-01 13:13:04 +0800

    initial commit

---
+++ C/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+	string name = get_string("Type in your name: ");
+	printf("Hello %s", name);
+	printf("\nHello, World!\n");
+	int i = get_int("What's your age?");
+}
