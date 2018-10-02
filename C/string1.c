502971d172c659c3579883b6b20072e8c9c09d1b
yourihdx <yourihdx@gmail.com>
2018-10-02 22:31:36 +0800

    strlen

---
+++ C/string1.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output:\n");
+
+    int n = strlen(s);
+    
+    for(int i = 0; i < n; i++)
+    {
+        printf("%c\n", s[i]);
+    }
+    
+}
