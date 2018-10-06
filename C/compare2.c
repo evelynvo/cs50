fff98deb516c57f31830c1a82833082a3ecb0dd2
yourihdx <yourihdx@gmail.com>
2018-10-06 22:56:32 +0800

    comparing strings with a boolean function

--- C/compare2.c
+++ C/compare2.c
@@ -3,7 +3,6 @@
 
 int main(void)
 {
-
     string s = get_string("s: ");
     string t = get_string("t: ");
 
@@ -19,9 +18,16 @@ int main(void)
 
 bool compare_strings(string a, string b)
 {
-    
     if (strlen(a) != strlen(b)) {
         return false;
     }
-    
+
+    for(int i = 0, n = strlen(a); i < n; i++)
+    {
+        
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
