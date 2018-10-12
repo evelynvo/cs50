7637042f03db420b4b9787ad329b67f42928b339
yourihdx <yourihdx@gmail.com>
2018-10-12 23:15:53 +0800

    adding the first error treatment

--- C/compare4.c
+++ C/compare4.c
@@ -8,8 +8,17 @@ int main(void)
 {   
     // Get two strings
     char *s = get_string("s: ");
+    
+    if (s == NULL) {
+        return 1;
+    }
+    
     char *t = get_string("t: ");
-
+    
+    if (t == NULL) {
+        return 1;
+    }
+    
     // Compare strings for equality
     if (strcmp(s, t) == 0)
     {
@@ -19,4 +28,5 @@ int main(void)
     {
         printf("different\n");
     }
+    return 0;
 }
