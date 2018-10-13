b7970c2945cdcf7d915dbd46fe2252aa3d08cb43
yourihdx <yourihdx@gmail.com>
2018-10-13 20:08:45 +0800

    allocating some memory for user input

---
+++ C/scanf1.c
@@ -0,0 +1,14 @@
+// Gets a string from user using scanf
+
+// this code is for example and does not work as it should
+
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = NULL;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+    return 0;
+}
---
+++ C/scanf2.c
@@ -0,0 +1,12 @@
+// Gets a string from user using scanf
+
+#include <stdio.h>
+
+int main(void)
+{
+    char s[6];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+    return 0;
+}
