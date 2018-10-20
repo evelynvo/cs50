72856abe458b859c5036d488a2895def3f86f195
yourihdx <yourihdx@gmail.com>
2018-10-20 22:05:22 +0800

    creating a list which is dynamically changing its' size

---
+++ C/list1.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int *numbers = NULL;
+    int capacity = 0;
+
+    int size = 0;
+
+    
+    while(true)
+    {
+        int number = get_int("Number: ");
+
+        if (number == INT_MAX) 
+        {   
+            printf("\n");
+            break;
+        }
+
+        if (size == capacity) // = If we have no more space
+        { 
+            numbers = realloc(numbers, sizeof(int) * (size + 1));
+            capacity++;     
+        }
+
+        numbers[size] = number;
+        size++;
+
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        printf("You inputted %i\n", numbers[i]);
+    }
+}
