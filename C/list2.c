255f95e1dfea8617ed067d4a9109bf795b03add5
yourihdx <yourihdx@gmail.com>
2018-10-29 21:10:06 +0800

    syncing folders

---
+++ C/list2.c
@@ -0,0 +1,60 @@
+// Implements a list of numbers using a linked list
+
+#include <cs50.h>
+#include <stdio.h>
+
+typedef 
+struct node
+{
+    
+    int number;
+    struct node *next;
+
+}
+node;
+
+int main(void)
+{
+    // Memory for numbers 
+    node *numbers = NULL;
+
+    // Prompt for numbers (Until EOF)
+    
+    while(true){
+        // Prompt for number
+        int number = get_int("number: ");
+
+        // Check for EOF
+        if (number == INT_MAX) {
+            break;
+        }
+
+        // Allocate space for number
+        node *n = malloc(sizeof(node));
+
+        if (!n) {
+            return 1;
+        }
+        
+        // Add number to the list
+        n->number = number;
+        n->next = NULL;
+
+        
+        if (numbers) {
+            
+            for(node *ptr = numbers; ptr != NULL; ptr = ptr->next)
+            {
+                if (ptr->next == NULL) {
+                    ptr->next = n;
+                    break;
+                }   
+            }   
+        } 
+        
+        else
+        {
+            numbers = n;
+        }  
+    }
+}
